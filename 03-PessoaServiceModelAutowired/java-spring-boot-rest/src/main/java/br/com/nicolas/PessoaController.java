4fb6b2a6e86d1811a34578e2fbacb31fd51493f4
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2023-09-14 22:18:47 -0300

    feat(PessoaController): mudando o retorno para no content

--- 03-PessoaServiceModelAutowired/java-spring-boot-rest/src/main/java/br/com/nicolas/PessoaController.java
+++ 03-PessoaServiceModelAutowired/java-spring-boot-rest/src/main/java/br/com/nicolas/PessoaController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -66,9 +67,10 @@ public class PessoaController {
 	}
 	
 	@DeleteMapping(path = "/{id}")
-	public void deleta(
+	public ResponseEntity<?> deleta(
 			@PathVariable(value = "id") Long id) {
 		pessoaService.deleta(id);
+		return ResponseEntity.noContent().build();
 	}
 	
 	
