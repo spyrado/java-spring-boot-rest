5c555a96e2969f793eebc8aee07b612324ecb336
Nicolas Guilherme Matheus <nicolas.gmop@gmail.com>
2023-09-19 08:03:19 -0300

    feat(PessoaVO): criando a model de VO

---
+++ 03-PessoaServiceModelAutowired/java-spring-boot-rest/src/main/java/br/com/nicolas/data/vo/v1/PessoaVO.java
@@ -0,0 +1,81 @@
+package br.com.nicolas.data.vo.v1;
+
+import java.io.Serializable;
+import java.util.Objects;
+
+public class PessoaVO implements Serializable {
+
+	private static final long serialVersionUID = 1L;
+
+
+	private Long id;
+	private String primeiroNome;
+	private String sobrenome;
+	private String endereco;
+	private String genero;
+	
+	public PessoaVO() {}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getPrimeiroNome() {
+		return primeiroNome;
+	}
+
+	public void setPrimeiroNome(String primeiroNome) {
+		this.primeiroNome = primeiroNome;
+	}
+
+	public String getSobrenome() {
+		return sobrenome;
+	}
+
+	public void setSobrenome(String sobrenome) {
+		this.sobrenome = sobrenome;
+	}
+
+	public String getEndereco() {
+		return endereco;
+	}
+
+	public void setEndereco(String endereco) {
+		this.endereco = endereco;
+	}
+
+	public String getGenero() {
+		return genero;
+	}
+
+	public void setGenero(String genero) {
+		this.genero = genero;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(endereco, genero, id, primeiroNome, sobrenome);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		PessoaVO other = (PessoaVO) obj;
+		return Objects.equals(endereco, other.endereco) && Objects.equals(genero, other.genero)
+				&& Objects.equals(id, other.id) && Objects.equals(primeiroNome, other.primeiroNome)
+				&& Objects.equals(sobrenome, other.sobrenome);
+	}
+	
+	
+	
+	
+}
